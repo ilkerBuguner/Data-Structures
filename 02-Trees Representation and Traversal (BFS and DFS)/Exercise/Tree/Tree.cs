7689054ea3538b8b7cb41b122e210c3301de9aad
İlker Bugüner <ilker.buguner@gmail.com>
2020-09-12 14:36:48 +0300

    Solved 3 problems from 02-Trees Representation and Traversal (BFS and DFS) lecture exercise.

--- 02-Trees Representation and Traversal (BFS and DFS)/Exercise/Tree/Tree.cs
+++ 02-Trees Representation and Traversal (BFS and DFS)/Exercise/Tree/Tree.cs
@@ -2,14 +2,26 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.Linq;
 
     public class Tree<T> : IAbstractTree<T>
     {
         private readonly List<Tree<T>> _children;
 
-        public Tree(T key, params Tree<T>[] children)
+        public Tree(T key)
         {
-            throw new NotImplementedException();
+            this.Key = key;
+            this.Parent = null;
+            this._children = new List<Tree<T>>();
+        }
+
+        public Tree(T key, params Tree<T>[] children) : this(key)
+        {
+            foreach (var child in children)
+            {
+                child.Parent = this;
+                this._children.Add(child);
+            }
         }
 
         public T Key { get; private set; }
@@ -22,12 +34,12 @@
 
         public void AddChild(Tree<T> child)
         {
-            throw new NotImplementedException();
+            this._children.Add(child);
         }
 
         public void AddParent(Tree<T> parent)
         {
-            throw new NotImplementedException();
+            this.Parent = parent;
         }
 
         public string GetAsString()
@@ -42,12 +54,52 @@
 
         public List<T> GetLeafKeys()
         {
-            throw new NotImplementedException();
+            var result = new List<T>();
+            var queue = new Queue<Tree<T>>();
+
+            queue.Enqueue(this);
+
+            while (queue.Count != 0)
+            {
+                var subtree = queue.Dequeue();
+
+                if(subtree._children.Count == 0)
+                {
+                    result.Add(subtree.Key);
+                }
+
+                foreach (var child in subtree.Children)
+                {
+                    queue.Enqueue(child);
+                }
+            }
+
+            return result;
         }
 
         public List<T> GetMiddleKeys()
         {
-            throw new NotImplementedException();
+            var result = new List<T>();
+            var queue = new Queue<Tree<T>>();
+
+            queue.Enqueue(this);
+
+            while (queue.Count != 0)
+            {
+                var subtree = queue.Dequeue();
+
+                if (subtree._children.Count > 0 && subtree.Parent != null)
+                {
+                    result.Add(subtree.Key);
+                }
+
+                foreach (var child in subtree.Children)
+                {
+                    queue.Enqueue(child);
+                }
+            }
+            result = result.OrderBy(x => x).ToList();
+            return result;
         }
 
         public List<T> GetLongestPath()
@@ -64,5 +116,6 @@
         {
             throw new NotImplementedException();
         }
+
     }
 }
--- 02-Trees Representation and Traversal (BFS and DFS)/Exercise/Tree/TreeFactory.cs
+++ 02-Trees Representation and Traversal (BFS and DFS)/Exercise/Tree/TreeFactory.cs
@@ -2,6 +2,7 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.Linq;
 
     public class TreeFactory
     {
@@ -14,22 +15,45 @@
 
         public Tree<int> CreateTreeFromStrings(string[] input)
         {
-            throw new NotImplementedException();
+            for (int i = 0; i < input.Length; i++)
+            {
+                var tokens = input[i].Split(' ').Select(int.Parse).ToArray();
+                var parentInt = tokens[0];
+                var childInt = tokens[1];
+
+                this.AddEdge(parentInt, childInt);
+            }
+
+            return this.GetRoot();
         }
 
         public Tree<int> CreateNodeByKey(int key)
         {
-            throw new NotImplementedException();
+            return new Tree<int>(key);
         }
 
         public void AddEdge(int parent, int child)
         {
-            throw new NotImplementedException();
+            var childTree = this.CreateNodeByKey(child);
+            if (this.nodesBykeys.ContainsKey(parent))
+            {
+                this.nodesBykeys[parent].AddChild(childTree);
+            }
+            else
+            {
+                var parentTree = this.CreateNodeByKey(parent);
+                this.nodesBykeys.Add(parent, parentTree);
+                this.nodesBykeys[parent].AddChild(childTree);
+            }
+
+            childTree.AddParent(this.nodesBykeys[parent]);
+            this.nodesBykeys.Add(child, childTree);
         }
 
         private Tree<int> GetRoot()
         {
-            throw new NotImplementedException();
+            var kvp = this.nodesBykeys.FirstOrDefault();
+            return kvp.Value;
         }
     }
 }
