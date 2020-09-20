8ea437a632c956332576a0be9b7796c62a21574f
İlker Bugüner <ilker.buguner@gmail.com>
2020-09-20 21:13:46 +0300

    Solved one more problem from Heaps BST lecture exercise.

--- 03-Heaps BST/Exercise/03.MinHeap/MinHeap.cs
+++ 03-Heaps BST/Exercise/03.MinHeap/MinHeap.cs
@@ -13,21 +13,107 @@
             this._elements = new List<T>();
         }
 
-        public int Size => throw new NotImplementedException();
+        public int Size => this._elements.Count;
 
         public T Dequeue()
         {
-            throw new NotImplementedException();
+            var firstElement = this.Peek();
+            this.EnsureNotEmpty();
+            this.Swap(0, this.Size - 1);
+            this._elements.RemoveAt(this.Size - 1);
+            this.HeapifyDown();
+
+            return firstElement;
         }
 
         public void Add(T element)
         {
-            throw new NotImplementedException();
+            this._elements.Add(element);
+
+            this.HeapifyUp();
         }
 
         public T Peek()
         {
-            throw new NotImplementedException();
+            this.EnsureNotEmpty();
+            return this._elements[0];
+        }
+
+        private void HeapifyUp()
+        {
+            int currentIndex = this.Size - 1;
+            int parentIndex = this.GetParentIndex(currentIndex);
+            while (this.IndexIsValid(currentIndex) && this.IsLess(currentIndex, parentIndex))
+            {
+                this.Swap(currentIndex, parentIndex);
+                currentIndex = parentIndex;
+                parentIndex = this.GetParentIndex(currentIndex);
+
+            }
+        }
+
+        private void HeapifyDown()
+        {
+            int index = 0;
+            int leftChildIndex = this.GetLeftChildIndex(0);
+            while (this.IndexIsValid(leftChildIndex) && this.IsLess(index, leftChildIndex))
+            {
+                int toSwapWith = leftChildIndex;
+                int rightChildindex = this.GetRightChildIndex(index);
+                if (this.IndexIsValid(rightChildindex) && this.IsLess(toSwapWith, rightChildindex))
+                {
+                    toSwapWith = rightChildindex;
+                }
+
+                this.Swap(toSwapWith, index);
+                index = toSwapWith;
+                leftChildIndex = this.GetLeftChildIndex(index);
+            }
+        }
+
+        private void Swap(int currentIndex, int parentIndex)
+        {
+            var temp = this._elements[currentIndex];
+            this._elements[currentIndex] = this._elements[parentIndex];
+            this._elements[parentIndex] = temp;
+        }
+
+        private bool IsGreater(int childIndex, int parentIndex)
+        {
+            return this._elements[childIndex].CompareTo(this._elements[parentIndex]) > 0;
+        }
+
+        private bool IsLess(int childIndex, int parentIndex)
+        {
+            return this._elements[childIndex].CompareTo(this._elements[parentIndex]) < 0;
+        }
+
+        private int GetParentIndex(int childIndex)
+        {
+            return (childIndex - 1) / 2;
+        }
+
+        private int GetLeftChildIndex(int parentIndex)
+        {
+            return 2 * parentIndex + 1;
+        }
+
+        private int GetRightChildIndex(int parentIndex)
+        {
+            return 2 * parentIndex + 2;
+        }
+
+        private bool IndexIsValid(int index)
+        {
+            return index > 0 && index < this.Size;
+        }
+
+        private void EnsureNotEmpty()
+        {
+            if (this.Size == 0)
+            {
+                throw new InvalidOperationException("Max heap is empty");
+            }
         }
     }
 }
