331270dced378734e0d3511fc0c51d4234342fc5
İlker Bugüner <ilker.buguner@gmail.com>
2020-10-03 14:08:19 +0300

    Solved every problem from Final Exam - 03.10.2020

---
+++ 05-Final Exam/01.Inventory/Inventory.cs
@@ -0,0 +1,170 @@
+﻿namespace _01.Inventory
+{
+    using _01.Inventory.Interfaces;
+    using _01.Inventory.Models;
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Inventory : IHolder
+    {
+        private List<IWeapon> weapons;
+
+        public Inventory()
+        {
+            this.weapons = new List<IWeapon>();
+        }
+
+        public int Capacity => this.weapons.Count;
+
+        public void Add(IWeapon weapon)
+        {
+            this.weapons.Add(weapon);
+        }
+
+        public void Clear()
+        {
+            this.weapons.Clear();
+        }
+
+        public bool Contains(IWeapon weapon)
+        {
+            return this.weapons.Contains(weapon);
+        }
+
+        public void EmptyArsenal(Category category)
+        {
+            foreach (var weapon in this.weapons)
+            {
+                if (weapon.Category == category)
+                {
+                    weapon.Ammunition = 0;
+                }
+            }
+        }
+
+        public bool Fire(IWeapon weapon, int ammunition)
+        {
+            var targetWeapon = this.weapons.Find(x => x == weapon);
+
+            if (targetWeapon == null)
+            {
+                throw new InvalidOperationException("Weapon does not exist in inventory!");
+            }
+
+            if (targetWeapon.Ammunition < ammunition)
+            {
+                return false;
+            }
+
+            targetWeapon.Ammunition -= ammunition;
+            return true;
+        }
+
+        public IWeapon GetById(int id)
+        {
+            var weapon = this.weapons.Find(x => x.Id == id);
+
+            if (weapon == null)
+            {
+                return null;
+            }
+
+            return weapon;
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            for (int i = 0; i < this.weapons.Count; i++)
+            {
+                yield return this.weapons[i];
+            }
+        }
+
+        public int Refill(IWeapon weapon, int ammunition)
+        {
+            var targetWeapon = this.weapons.Find(x => x == weapon);
+
+            if (targetWeapon == null)
+            {
+                throw new InvalidOperationException("Weapon does not exist in inventory!");
+            }
+
+            if (targetWeapon.Ammunition + ammunition >= targetWeapon.MaxCapacity)
+            {
+                targetWeapon.Ammunition = targetWeapon.MaxCapacity;
+            }
+            else
+            {
+                targetWeapon.Ammunition += ammunition;
+            }
+
+            return targetWeapon.Ammunition;
+        }
+
+        public IWeapon RemoveById(int id)
+        {
+            var targetWeapon = this.weapons.Find(x => x.Id == id);
+
+            if (targetWeapon == null)
+            {
+                throw new InvalidOperationException("Weapon does not exist in inventory!");
+            }
+
+            this.weapons.Remove(targetWeapon);
+            return targetWeapon;
+        }
+
+        public int RemoveHeavy()
+        {
+            var counter = 0;
+
+            for (int i = 0; i < this.weapons.Count; i++)
+            {
+                var currentWeapon = this.weapons[i];
+                if (currentWeapon.Category == Category.Heavy)
+                {
+                    this.weapons.Remove(currentWeapon);
+                    i--;
+                    counter++;
+                }
+            }
+
+            return counter;
+        }
+
+        public List<IWeapon> RetrieveAll()
+        {
+            if (this.weapons.Count <= 0)
+            {
+                return new List<IWeapon>();
+            }
+
+            return new List<IWeapon>(this.weapons);
+        }
+
+        public List<IWeapon> RetriveInRange(Category lower, Category upper)
+        {
+            return this.weapons.Where(x => x.Category >= lower && x.Category <= upper).ToList();
+        }
+
+        public void Swap(IWeapon firstWeapon, IWeapon secondWeapon)
+        {
+            var firstWeaponIndex = this.weapons.IndexOf(firstWeapon);
+            var secondWeaponIndex = this.weapons.IndexOf(secondWeapon);
+
+            if (firstWeaponIndex == -1 || secondWeaponIndex == -1)
+            {
+                throw new InvalidOperationException("Weapon does not exist in inventory!");
+            }
+
+            if (firstWeapon.Category == secondWeapon.Category)
+            {
+                var temp = this.weapons[firstWeaponIndex];
+                this.weapons[firstWeaponIndex] = this.weapons[secondWeaponIndex];
+                this.weapons[secondWeaponIndex] = temp;
+            }
+        }
+    }
+}
---
+++ 05-Final Exam/02.LegionSystem/Legion.cs
@@ -0,0 +1,107 @@
+﻿namespace _02.LegionSystem
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Security.Cryptography.X509Certificates;
+    using _02.LegionSystem.Interfaces;
+    using Wintellect.PowerCollections;
+
+    public class Legion : IArmy
+    {
+        private OrderedSet<IEnemy> legion;
+
+        public Legion()
+        {
+            this.legion = new OrderedSet<IEnemy>();
+        }
+
+        public int Size => this.legion.Count;
+
+        public bool Contains(IEnemy enemy)
+        {
+            return this.legion.Contains(enemy);
+        }
+
+        public void Create(IEnemy enemy)
+        {
+            this.legion.Add(enemy);
+        }
+
+        public IEnemy GetByAttackSpeed(int speed)
+        {
+            IEnemy targetEnemy = null;
+            foreach (var enemy in this.legion)
+            {
+                if (enemy.AttackSpeed == speed)
+                {
+                    targetEnemy = enemy;
+                }
+            }
+
+            return targetEnemy;
+        }
+
+        //Can be refactored
+        public List<IEnemy> GetFaster(int speed)
+        {
+            return this.legion.Where(e => e.AttackSpeed > speed).ToList();
+        }
+
+        public IEnemy GetFastest()
+        {
+            if (this.Size <= 0)
+            {
+                throw new InvalidOperationException("Legion has no enemies!");
+            }
+
+            return this.legion.GetFirst();
+        }
+
+        public IEnemy[] GetOrderedByHealth()
+        {
+            if (this.Size <= 0)
+            {
+                return new IEnemy[0];
+            }
+
+            return this.legion.OrderByDescending(x => x.Health).ToArray();
+        }
+
+        //Can be refactored
+        public List<IEnemy> GetSlower(int speed)
+        {
+            return this.legion.Where(e => e.AttackSpeed < speed).ToList();
+        }
+
+        public IEnemy GetSlowest()
+        {
+            if (this.Size <= 0)
+            {
+                throw new InvalidOperationException("Legion has no enemies!");
+            }
+
+            return this.legion.GetLast();
+        }
+
+        public void ShootFastest()
+        {
+            if (this.Size <= 0)
+            {
+                throw new InvalidOperationException("Legion has no enemies!");
+            }
+
+            this.legion.RemoveFirst();
+        }
+
+        public void ShootSlowest()
+        {
+            if (this.Size <= 0)
+            {
+                throw new InvalidOperationException("Legion has no enemies!");
+            }
+
+            this.legion.RemoveLast();
+        }
+    }
+}
